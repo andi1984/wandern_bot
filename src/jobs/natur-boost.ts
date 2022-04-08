09fad345d388cacd1bf6b3be80e34e8549404ad0
Andreas Sander <mail@andi1984.de>
2022-04-08 22:44:15 +0200

    fix: async forEach

--- src/jobs/natur-boost.ts
+++ src/jobs/natur-boost.ts
@@ -11,10 +11,16 @@ import login from "../helper/login";
   //Async iterable
   const result: { value: { id: string }[] } = await results.next();
 
+  const asyncForEach = async <K>(
+    arr: K[],
+    predicate: (value: K, index: number, array: K[]) => Promise<boolean>
+  ) => await Promise.all(arr.map(predicate));
+
   // We got our first X entries in result.value
   // Reblog/Boost all natur posts
-  result.value.forEach((post) => {
-    mastoInstance.statuses.reblog(post.id, { visibility: "public" });
+  await asyncForEach(result.value, async (post) => {
+    await mastoInstance.statuses.reblog(post.id, { visibility: "public" });
+    return true;
   });
 
   // signal to parent that the job is done
