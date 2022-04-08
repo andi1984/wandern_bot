9902b64344762541b1fc0204b2595d5a3670ef42
Andreas Sander <mail@andi1984.de>
2022-04-08 22:24:55 +0200

    chore: Decrease job interval

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const bree = new Bree({
   jobs: [
     { name: "feed-grabber", cron: "0 22 * * *" },
     { name: "feed-tooter", cron: "0 16 */2 * *" },
-    { name: "natur-boost", cron: "0 14 * * *" },
+    { name: "natur-boost", interval: "10m" },
     { name: "alive", interval: "30m" },
   ],
 });
